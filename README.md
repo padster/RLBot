e4c5a8c0c2ae8b638fbef51a8f0761f9d697bc13
Pat Coleman <padsterpat@gmail.com>
2018-01-23 20:15:45 -0800

    Add in client code: Unity app plus network handling to update it live.

--- README.md
+++ README.md
@@ -1,7 +1,51 @@
-# RLVR (Modified RLBot)
+# RLVR: Soccar in full 3D virtual reality
 
-See [drssoccer55's original RLBot](https://github.com/drssoccer55/RLBot) for details on how to get the RL Bot working.
-The config edits here will run a match with three cars:
+### What‽
+[Rocket League](https://www.rocketleague.com/) mixes cars and soccer but is currently only played on a normal 2D screen. RLVR is an experiment to bring Rocket League to its proper platform, full 3D virtual reality! Be an egg, and drive your rocket-boosted car around the map, seeing the environment live on a VR headset.
+
+### How?
+Not gonna lie, this requires a bit of setup... You'll need Rocket League, python, Unity 3D, the Google Cardboard Unity SDK, plus a cardboard-style viewer (e.g. Google Cardboard, Utopia 360, etc... there are [lots](https://www.amazon.com/Cell-Phone-VR-Headsets/b?node=14775002011)) and an Android phone to put in it.
+
+##### 1) Getting the server working
+a) Install Rocket League (surprise...)
+b) See [drssoccer55's original RLBot](https://github.com/drssoccer55/RLBot) for details on how to get the RL Bot working.
+After checking out this repo locally, you'll need to launch RL, then patch the .dll with RLBot_Injector.
+c) run `python runner.py` to launch RL bot into a game. The config edits here will run a match with three cars:
 - you
-- botserver opponent which will send game data to local port 3451 
+- `botserver.py` opponent which will send game data to local port 3451 
 - a normal all-star bot opponent 
+
+At this stage you can verify it works by also running `python testClient.py` - that will print out game state very quickly to console.
+
+##### 2) Get the client working
+a) Install the [Unity 3D](https://unity3d.com/unity) app; the personal version should be fine, and free!
+b) Install the [Google VR Unity packages](https://developers.google.com/vr/develop/unity/get-started) - they are used in the app, but not included in this repository.
+d) Select GameState, and change the local IP property to be that of the computer your server is running on (try checking `ipconfig`, it might look something like `192.168.x.y`)
+e) If your server from part 1 is working, you can hit play in Unity and it should connect and start drawing the game inside the Unity UI.
+
+Assuming the above works, you can install it on your phone:
+f) Attach your phone to your computer, and turn on USB debugging.
+g) File -> Build & Run will compile it to an .apk and run it on your phone.
+h) Put your phone in the headset, and enjoy! 
+
+Once fully working, you should be able to still control your car as normal on server machine running Rocket League. You'll still see the game there, but it'll also be rendered to the headset.
+
+##### Notes
+Note that if the server or client run into problems, everything will probably break. You can only connect one client at a time, and stopping it will require restarting the server too; so the eventual pattern will be:
+* Run Rocket League
+* Run RLBot_Injector to inject bot code
+* Run `python runner.py` to start the server
+* Run the RLVR app on your phone to connect and display
+* Play RL on the server machine, but watch in VR :)
+
+
+##### Future work
+Not much effort has been put into making the game world look nice in VR. Some things I'll add if I get the time, or up for grabs if people want to beat me to it:
+* Add basic HUD: Boost counter, game timer and score
+* Use proper 3D models for the field, ball and cars.
+* Switch to UDP rather than TCP sockets.
+* Fix bugs: 3D rotation doesn't work well on the walls
+* Get the server bot to spectate, or worst case, drive inside the goals.
+
+
+If you're interested in playing around with the code to get your own stuff working, take a look at `botServer.py` on the sending side and `listener.cs` on the receiving side. So long as those two stay in sync, you can send any game state you like from the bot to the Unity app, and use it to update the VR game world.
---
+++ UnityApp/Assets/listener.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using UnityEngine;
+using UnityEngine.UI;
+
+/**
+ * Script that runs during the game, pulls state from botServer, and updates the UI.
+ */
+public class listener : MonoBehaviour {
+    // Networking constants
+    private static int BUFF_LEN = 2048; // 2kb, just in case the network is backed up? :/
+    private static int BYTES_10 = 8; // 2 ints (8 bytes) of boundary 1010...
+    private static int EXPECTED_PACKET_BYTES = // Make sure this matches up botServer.py
+        BYTES_10
+        + 3 * 4 // ball x/y/z
+        + 3 * 4 + 3 * 4 // your x/y/z pitch/yaw/roll
+        + 3 * 4 + 3 * 4; // bot x/y/z pitch/yaw/roll
+    
+    // Connection, and thing to write to.
+    private NetworkStream clientStreamReader;
+    private byte[] buffer = new byte[BUFF_LEN];
+
+    // Address and port for the server. Set these from the Unity UI.
+    public string localIP = "localhost";
+    public int localPort = 3451;
+
+    // Game objects to move around.
+    public GameObject ball;
+    public GameObject playerCar;
+    public GameObject botCar;
+
+    // Called at the start, do a blocking connect to the server.
+	void Start () {
+        try {
+            Debug.Log("RLVR Connecting to " + localIP + ":" + localPort + "...");
+            // TODO: UDP instead?
+            TcpClient tcpClient = new TcpClient();
+            tcpClient.Connect(localIP, localPort);
+            clientStreamReader = tcpClient.GetStream();
+            Debug.Log("RLVR Connected!");
+        } catch (System.Exception e) {
+            Debug.Log(e.Message);
+            Debug.Log("RLVR Failed :(");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (clientStreamReader == null) {
+            return;
+        }
+
+        // Read a packet of network traffic, and find the most recent full update.
+        int nRead = clientStreamReader.Read(buffer, 0, BUFF_LEN);
+        int at = findLastPacketStart(buffer, nRead);
+        if (at == -1) {
+            Debug.Log("Whoops, couldn't find valid packet boundary :(");
+        }
+
+        // Parse the values back out of the update...
+        Vector3  ballPos = readLocation(buffer, at); at += 12;
+        Vector3    p0Pos = readLocation(buffer, at); at += 12;
+        Quaternion p0Rot = readRotation(buffer, at); at += 12;
+        Vector3    p1Pos = readLocation(buffer, at); at += 12;
+        Quaternion p1Rot = readRotation(buffer, at); at += 12;
+
+        // ...and update the world
+        ball.transform.position = ballPos;
+        playerCar.transform.SetPositionAndRotation(p0Pos, p0Rot);
+        botCar.transform.SetPositionAndRotation(p1Pos, p1Rot);
+	}
+
+    // Read an X/Y/Z from the server, convert to Unity space.
+    Vector3 readLocation(byte[] buffer, int at) {
+        float x = System.BitConverter.ToSingle(buffer, at + 0);
+        float y = System.BitConverter.ToSingle(buffer, at + 4);
+        float z = System.BitConverter.ToSingle(buffer, at + 8);
+        return new Vector3(x, z, -y) / 1000.0f;
+    }
+
+    // Read Pitch/Yaw/Roll from the server, convert to a Unity rotation.
+    Quaternion readRotation(byte[] buffer, int at) {
+        int pitch = System.BitConverter.ToInt32(buffer, at + 0);
+        int yaw   = System.BitConverter.ToInt32(buffer, at + 4);
+        int roll  = System.BitConverter.ToInt32(buffer, at + 8);
+        Vector3 rotation = new Vector3(-roll, yaw, pitch) * (180.0f / 32768.0f);
+        // TODO: fix - this version is wrong on the walls.
+        return Quaternion.Euler(rotation);
+    }
+
+    // Given a buffer of data, walk backwards from the end until we find the boundary marker.
+    int findLastPacketStart(byte[] buffer, int nRead){
+        int at = 0, inARow = 0;
+        for (at = nRead - (EXPECTED_PACKET_BYTES - BYTES_10) - 1; at >= 0 && inARow < BYTES_10; at--) {
+            inARow = (buffer[at] == 85 ? inARow + 1 : 0);
+        }
+        if (at == -1 && inARow < BYTES_10) {
+            Debug.Log("Whoops, couldn't find packet boundary :(");
+            return -1;
+        }
+        at++;
+
+        int boundaryCount = 0;
+        for (int i = 0; i < BYTES_10 && at + 1 < nRead; i++, at++) {
+            boundaryCount += (buffer[at] == 85 ? 1 : 0);
+        }
+        Debug.Assert(boundaryCount == BYTES_10, "Boundary found but not? what?");
+        return at;
+    }
+
+}
